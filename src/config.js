c6a80a5c540cc2af9ad3775367f2b54ca7b5491b
juliocesarrojas86 <julio.rojasusb@gmail.com>
2024-07-07 10:07:30 -0500

    config url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     api: {
-        baseURL: "http://localhost:5000"
+        //baseURL: "http://localhost:5000"
+        baseURL: "https://backendg80-1-wk8q.onrender.com"
     }
 }
